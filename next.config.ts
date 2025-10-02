831c2e99e32e94a0ca580d06e47e2cf03cfede40
zesuperaker <bobafettskywalker@gmail.com>
2025-10-02 04:13:07 -0700

    1.20

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,26 @@
-import type { NextConfig } from 'next'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
+}
 
-const config: NextConfig = {}
-
-export default config
+module.exports = nextConfig
